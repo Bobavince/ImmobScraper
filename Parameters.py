4cf3628a2678d09b91e7cafdb239a2fc6931914d
Thibault Douzon <douzont@gmail.com>
2019-10-21 22:19:54 +0200

    black formatting

--- Parameters.py
+++ Parameters.py
@@ -1,11 +1,9 @@
 ## LEBONCOIN
 # Search URL
-url_leboncoin = 'YOUR QUERY'
-page_add_leboncoin = '&page='  # '&page=2'
+url_leboncoin = "YOUR QUERY"
+page_add_leboncoin = "&page="  # '&page=2'
 
 ## SE LOGER
 # Search URL
-url_seloger = 'YOUR QUERY'
-page_add_seloger = '&LISTING-LISTpg='  # '&LISTING-LISTpg=2'
-
-
+url_seloger = "YOUR QUERY"
+page_add_seloger = "&LISTING-LISTpg="  # '&LISTING-LISTpg=2'
--- scrape_all.py
+++ scrape_all.py
@@ -1,9 +1,9 @@
+from Parameters import (page_add_leboncoin, page_add_seloger, url_leboncoin,
+                        url_seloger)
 from scraper_leboncoin import LebonCoin_Scraper
 from scraper_seloger import SeLoger_Scraper
-from Parameters import url_leboncoin, page_add_leboncoin
-from Parameters import url_seloger, page_add_seloger
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     scraper_1 = LebonCoin_Scraper(url_leboncoin, page_add_leboncoin)
     list_appt_1 = scraper_1.get_full_list_appt()
 
